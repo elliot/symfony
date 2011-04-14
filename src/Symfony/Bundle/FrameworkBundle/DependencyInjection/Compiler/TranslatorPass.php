d2aa6c4900126df00dcb31e28bffc53597568269
Elliot Anderson <elliot.a@gmail.com>
2011-04-14 22:13:31 +1000

    Proposed fix for issue #9650. Need advice on how to handle unrecognised formats

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/TranslatorPass.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Compiler/TranslatorPass.php
@@ -13,10 +13,26 @@ class TranslatorPass implements CompilerPassInterface
             return;
         }
 
+        // Compile loaders
         $loaders = array();
         foreach ($container->findTaggedServiceIds('translation.loader') as $id => $attributes) {
             $loaders[$id] = $attributes[0]['alias'];
         }
         $container->setParameter('translation.loaders', $loaders);
+        
+        // Drop resources that don't have a laoder to stop an exception being thrown
+        $uncheckedResources = array();
+        
+        $resources = $container->getParameter('translation.resources');
+        foreach ($resources as $resource) {
+            if (! in_array($resource[0], $loaders)) {
+                // The format does not ahve a registered loader
+                // How to handle??
+            } else {
+                $checkedResources[] = $resource;
+            }
+        }
+        
+        $container->setParameter('translation.resources', $checkedResources);
     }
 }
